8fa0346f86a2708de61bac19de2494075a03720b
Takuya Matsuyama <hi@craftz.dog>
2023-12-14 14:36:48 +0900

    fix(link): use the regular link for external urls

--- src/components/ThumbLinks.jsx
+++ src/components/ThumbLinks.jsx
@@ -1,6 +1,6 @@
 'use client'
 
-import Link from 'next/link'
+import NextLink from 'next/link'
 import Image from 'next/image'
 import { navigation } from '@/lib/navigation'
 import { Icon } from '@/components/Icon'
@@ -8,6 +8,7 @@ import { Icon } from '@/components/Icon'
 export function ThumbLink({ title, href, cover }) {
   const isVideo = href.startsWith('https://www.youtube.com')
   const Img = cover.startsWith('/') ? Image : 'img'
+  const Link = href.startsWith('https://') ? 'a' : NextLink
   return (
     <div className="group relative rounded-xl border border-slate-200 dark:border-slate-800">
       <div className="absolute -inset-px rounded-xl border-2 border-transparent opacity-0 [background:linear-gradient(var(--quick-links-hover-bg,theme(colors.sky.50)),var(--quick-links-hover-bg,theme(colors.sky.50)))_padding-box,linear-gradient(to_top,theme(colors.indigo.400),theme(colors.cyan.400),theme(colors.sky.500))_border-box] group-hover:opacity-100 dark:[--quick-links-hover-bg:theme(colors.slate.800)]" />
